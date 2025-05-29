9f4667ca7d76474fead6d27205c6a483e26bd97c
David Melquíades <davidmelki08@gmail.com>
2025-05-29 08:33:01 -0300

    Aula e exercicios sobre typescript

---
+++ exercicios-ts/exercicio1.js
@@ -0,0 +1,6 @@
+var produto = 'Mouse';
+var preco = 59.90;
+var disponivel = true;
+console.log('produto: ${produto}');
+console.log('preco: ${preco}');
+console.log('disponivel: ${disponivel}');
---
+++ exercicios-ts/exercicio1.ts
@@ -0,0 +1,6 @@
+let produto: string = 'Mouse';
+let preco: number = 59.90;
+let disponivel: boolean = true;
+console.log('produto: ${produto}');
+console.log('preco: ${preco}');
+console.log('disponivel: $disponivel}');
